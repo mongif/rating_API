c510ba763d6aad6c66eb9464c2f8733ed6f56f5f
xfiendx4life <xfiendx4life@gmail.com>
2018-12-05 13:37:40 +0300

    File upload created

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 from jinja2 import Template
 import jinja2
 import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['xlsx'])
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates' )
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
@@ -15,9 +26,25 @@ def render_str(template,**params):
 def render(template, **kw):
    return render_str(template, **kw)
 
-@app.route("/")
+@app.route("/" , methods=['GET', 'POST'])
 def hello():
-    return render("index.html")
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',filename=filename))
+	else:		
+		return render("index.html")
 
+@app.route("/uploaded_file:<filename>" , methods=['GET', 'POST'])
+def uploaded_file(filename):
+	return render('success.html')
 if __name__ == '__main__':
 	app.run(debug = True)
